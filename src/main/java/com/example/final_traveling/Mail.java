8e39f73b5ba976760b8a968552482f621108a6cc
Abdulmajid <maajidawol@gmail.com>
2023-06-08 22:56:03 +0300

    --

--- src/main/java/com/example/final_traveling/Mail.java
+++ src/main/java/com/example/final_traveling/Mail.java
@@ -27,7 +27,7 @@ public class Mail {
         try {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("tourmanagementsystemseca@gmail.com"));
+            message.setFrom(new InternetAddress("senderemail@gmail.com"));
             message.setRecipients(
                     Message.RecipientType.TO,
                     InternetAddress.parse(to)
